23801e4a99da0fc0b8c9468bc893a6a1176986f6
alexamun <alecuccurullo@gmiail.com>
2018-07-06 12:38:57 +0200

    changed redirect destination on logout from signinup.jsp to index.jsp

--- src/it/focus/controller/Logout.java
+++ src/it/focus/controller/Logout.java
@@ -13,26 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-   
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		Cookie usrcookie = new Cookie("usr" , "");
 		usrcookie.setMaxAge(0);
 		usrcookie.setPath("/");
 		response.addCookie(usrcookie);
-		
 
-		
+
+
 		Cookie rolecookie = new Cookie("role" , "");
 		rolecookie.setMaxAge(0);
 		rolecookie.setPath("/");
 		response.addCookie(rolecookie);
-		
+
 		Cookie namecookie = new Cookie("name" , "");
 		namecookie.setMaxAge(0);
 		namecookie.setPath("/");
 		response.addCookie(namecookie);
-		
+
 		Cookie surnamecookie = new Cookie("surname" , "");
 		surnamecookie.setMaxAge(0);
 		surnamecookie.setPath("/");
@@ -42,17 +42,17 @@ public class Logout extends HttpServlet {
 		emailcookie.setMaxAge(0);
 		emailcookie.setPath("/");
 		response.addCookie(emailcookie);
-		
+
 		Cookie phonecookie = new Cookie("phone" , "");
 		phonecookie.setMaxAge(0);
 		phonecookie.setPath("/");
 		response.addCookie(phonecookie);
-		
+
 		request.getSession().invalidate();
-		response.sendRedirect("./singinup.jsp");
+		response.sendRedirect("./index.jsp");
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
