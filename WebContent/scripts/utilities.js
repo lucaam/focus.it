a6e8e9885483421857c48933cc6c4635e2f57275
Luca <luca.amoriello@hotmail.it>
2018-07-10 11:10:52 +0200

    Add to cart animation instead of page refresh

--- WebContent/scripts/utilities.js
+++ WebContent/scripts/utilities.js
@@ -6,13 +6,38 @@ function added(id){
 	};
 	xhttp.open("GET", "addToCart?idProd=" + id + "", true);
 	xhttp.send();
-	if(xhttp){
+	if(xhttp.readyState !=0){
 		$("#added").css("color", "green");
-		$("#added").append("Aggiunto al carrello");
+		$("#added").text("Added");
 		$("#added").fadeIn(700);
 		$("#added").fadeOut(2100);
-		
-		
+	}
+}
 
+function addFromSearch(id){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystateChange = function(){
+		if(xhttp.readyState == 4 && xhttp.status == 200){	
+		}
+	};
+	xhttp.open("GET", "addToCart?idProd=" + id + "", true);
+	xhttp.send();
+	if(xhttp.readyState !=0){
+
+			$("#added"+id).css("color", "green");
+			$("#added"+id).text("Added");
+			$("#added"+id).fadeIn(700);
+			$("#added"+id).fadeOut(2100);
+			
+			setTimeout(
+					function() 
+					  {
+						$("#added"+id).css("color", "white");
+						$("#added"+id).text("Cart");
+						$("#added"+id).fadeIn(150);
+					  }, 2100);
+			
+	
+		
 	}
-	}
+}
